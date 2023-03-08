98d69f57eb35b5326737ba7b7e045ce069e71b19
psong1 <paulsong29@gmail.com>
2023-03-07 21:44:41 -0500

    added manager to index

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-const html = require
+// require html.js to create page
+const html = require('./src/html')
+
+// require team information for cards
+const Manager = require('.lib/manager');
+const Intern = require('./lib/intern');
+const Engineer = require('./lib/engineer');
+const fs = require('fs');
+const inquirer = require('inquirer');
+const { mainModule } = require('process');
+
+const team = [];
+
+const generateManager = () => {
+    return inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'name',
+            message: `What is the manager's name?`,
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: `What is the manager's ID?`,
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: `What is the manager's email?`,
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: `What is the manager's office number?`,
+        }
+    ])
+    .then(managerInput => {
+        const { name, id, email, officeNumber } = managerInput;
+        const manager = new Manager (name, id, email, officeNumber);
+        team.push(manager);
+    })
+};
+
